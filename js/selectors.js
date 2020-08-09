34df8ca85bfeffb075bb3d565888bdb468f78889
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-09 21:24:49 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  $('img:last')
+}
+
+function ninjaBabySelector() {
+  $('#baby-ninja')
+}
+
+function divSelector() {
+  $('.pics')
+}
+
+function firstListItem() {
+  $('ul #pic-list:first-child')
+}
